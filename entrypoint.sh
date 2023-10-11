46e0123f351f35533da1ae76856dc437e1b1f781
Pierrick Delrieu <58939886+pierrickdelrieu@users.noreply.github.com>
2023-10-11 10:41:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,12 +21,18 @@ SCP_OPTIONS="-i $SSH_KEY -p "$SSH_PORT""
 #fi
 
 
-echo "$SSH_KEY" > /root/ssh_key
-chmod 600 /root/ssh_key
-#echo "$SSH_PASSPHRASE" > /root/ssh_passphrase
-#chmod 600 /root/ssh_passphrase
+# Create an SSH key file with the provided key and passphrase
+mkdir -p $HOME/.ssh
+echo "$SSH_KEY" > $HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_rsa
+
+# Add passphrase to the SSH agent
+if [ -n "$SSH_PASSPHRASE" ]; then
+  echo "$SSH_PASSPHRASE" | ssh-add -
+fi
 
 # Copy files to the remote server
 #sshpass -p /root/ssh_passphrase scp -P "${SSH_PORT}" -i /root/ssh_key -o StrictHostKeyChecking=no -o ConnectTimeout=30 -o ServerAliveInterval=30 -r "$GITHUB_WORKSPACE/${SOURCE_DIR}" "${SSH_USERNAME}@${HOST}:${DESTINATION_DIR}"
-sshpass -p ${SSH_PASSPHRASE} scp -P "${SSH_PORT}" -i /root/ssh_key -o StrictHostKeyChecking=no -o ConnectTimeout=30 -o ServerAliveInterval=30 -r "$GITHUB_WORKSPACE/${SOURCE_DIR}" "${SSH_USERNAME}@${HOST}:${DESTINATION_DIR}"
+#sshpass -p ${SSH_PASSPHRASE} scp -P "${SSH_PORT}" -i /root/ssh_key -o StrictHostKeyChecking=no -o ConnectTimeout=30 -o ServerAliveInterval=30 -r "$GITHUB_WORKSPACE/${SOURCE_DIR}" "${SSH_USERNAME}@${HOST}:${DESTINATION_DIR}"
+scp -P "${SSH_PORT}" -o StrictHostKeyChecking=no -o ConnectTimeout=30 -o ServerAliveInterval=30 -r "$GITHUB_WORKSPACE/${SOURCE_DIR}" "${SSH_USERNAME}@${HOST}:${DESTINATION_DIR}"
 
